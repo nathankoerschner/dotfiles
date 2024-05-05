28deaa3bb217aca54f6be987503a711eb06f83ac
nathankoerschner <nathankoerschner@gmail.com>
2024-05-04 23:57:00 -0400

    Include [D]elete [T]his [F]ile mapping

--- .config/nvim/lua/customized/remap.lua
+++ .config/nvim/lua/customized/remap.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>pq", ":silent !sqlfluff fix % --dialect snowflake<CR><CR>", { silent = true })
-vim.keymap.set("n", "<leader><C-d><C-f>", ":!rm %<CR>:q<CR>")
+vim.keymap.set("n", "<leader><C-d><C-t><C-f>", ":!rm % <CR> :q<CR>")
 -- vim.keymap.set("n", "<leader><C-f>", ":silent !tmux new-window tmux-sessionizer<CR>", { silent = true })
 -- vim.keymap.set("n", "<leader><C-d><C-f>", ":!tmux new-window | shv -p '/Users/nathankoerschner/Documents/'<CR>")
 
