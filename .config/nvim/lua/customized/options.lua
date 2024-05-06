c14ccf9bfe248828795e83b430d64c94cbbaa847
nathankoerschner <nathankoerschner@gmail.com>
2024-05-05 22:30:43 -0400

    nvim remaps & prefs

--- .config/nvim/lua/customized/options.lua
+++ .config/nvim/lua/customized/options.lua
@@ -4,10 +4,30 @@ vim.opt.number=true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.opt.autoindent = true
+vim.opt.smartindent = true
 
 vim.opt.ignorecase = true
 vim.opt.smartcase = true -- if search is mixed case, then be case-sensitive
 
 vim.opt.splitright = true
 vim.opt.splitbelow = true
+
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 12
+vim.opt.signcolumn = "yes"
+
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
--- .config/nvim/lua/customized/remap.lua
+++ .config/nvim/lua/customized/remap.lua
@@ -3,6 +3,37 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>pq", ":silent !sqlfluff fix % --dialect snowflake<CR><CR>", { silent = true })
 vim.keymap.set("n", "<leader><C-d><C-t><C-f>", ":!rm % <CR> :q<CR>")
--- vim.keymap.set("n", "<leader><C-f>", ":silent !tmux new-window tmux-sessionizer<CR>", { silent = true })
+vim.keymap.set("n", "<leader><C-f>", ":silent !tmux new-window tmux-sessionizer<CR>", { silent = true })
 -- vim.keymap.set("n", "<leader><C-d><C-f>", ":!tmux new-window | shv -p '/Users/nathankoerschner/Documents/'<CR>")
+--
 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+
+vim.keymap.set("n", "J", "mzJ`z")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+
+-- Remaps to skip yanking of text in common scenarios
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set("n", "Q", "<nop>") -- disable ex mode
+
+-- start renaming selected word
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
