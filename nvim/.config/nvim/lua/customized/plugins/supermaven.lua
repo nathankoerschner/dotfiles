db12356a605d1d514286e1a8b908bc14679242dc
NathanKoerschner <nathankoerschner@gmail.com>
2024-07-03 10:35:13 -0400

    Config supermaven

--- nvim/.config/nvim/lua/customized/plugins/supermaven.lua
+++ nvim/.config/nvim/lua/customized/plugins/supermaven.lua
@@ -1,6 +1,20 @@
 return {
   "supermaven-inc/supermaven-nvim",
   config = function()
-    require("supermaven-nvim").setup({})
+    require("supermaven-nvim").setup({
+      keymaps = {
+        accept_suggestion = "<Tab>",
+        clear_suggestion = "<C-]>",
+        accept_word = "<C-j>",
+      },
+      ignore_filetypes = { md = true, markdown = true, csv = true },
+      color = {
+        suggestion_color = "#ffffff",
+        cterm = 244,
+      },
+      log_level = "info", -- "off", or set to "info" to disable logging 
+      disable_inline_completion = false, -- disables inline completion for use with cmp
+      disable_keymaps = false, -- disables built in keymaps for more manual control
+    })
   end,
 }
