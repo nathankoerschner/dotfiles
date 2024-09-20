a76a0788f305988eda68753aa94ea17501cc12d1
daasity-nathan <nathan@daasity.com>
2024-09-20 17:03:35 -0400

    Oil.nvim, remove hartime.nmim

--- nvim/dot-config/nvim/lua/customized/remap.lua
+++ nvim/dot-config/nvim/lua/customized/remap.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 vim.keymap.set("n", "<leader>pq", ":silent !sqlfluff fix % --dialect snowflake<CR><CR>", { silent = true })
 vim.keymap.set("n", "<leader><C-d><C-t><C-f>", ":!rm % <CR> :q<CR>")
 vim.keymap.set("n", "<leader><C-f>", ":silent !tmux new-window tmux-sessionizer<CR>", { silent = true })
