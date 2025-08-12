885c5066a26221ab431c051920028cf6cd3a2c6e
NathanKoerschner <nathankoerschner@gmail.com>
2025-08-12 12:28:14 -0400

    Update location of save

--- nvim/dot-config/nvim/lua/customized/plugins/img-clip.lua
+++ nvim/dot-config/nvim/lua/customized/plugins/img-clip.lua
@@ -4,9 +4,19 @@ return {
   opts = {
     -- add options here
     --
-    dir_path = "~/org/.assets",
-    use_absolute_path = true,
+    default = {
+      -- make dir_path relative to current file rather than the cwd
+      -- To see your current working directory run `:pwd`
+      -- So if this is set to false, the image will be created in that cwd
+      -- In my case, I want images to be where the file is, so I set it to true
+      relative_to_current_file = true, ---@type boolean
 
+      -- I want to save the images in a directory named after the current file,
+      -- but I want the name of the dir to end with `-img`
+      dir_path = './',
+      show_dir_path_in_prompt = true,
+      file_name = ".%Y-%m-%d-at-%H-%M-%S",
+    },
     -- or leave it empty to use the default settings
   },
   keys = {
