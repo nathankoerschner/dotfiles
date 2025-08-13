c40e690e72c637b3f080d21d2897d432369dcf16
daasity-nathan <nathan@daasity.com>
2025-08-13 17:02:02 -0400

    Update orgmode settings

--- nvim/dot-config/nvim/lua/customized/plugins/orgmode.lua
+++ nvim/dot-config/nvim/lua/customized/plugins/orgmode.lua
@@ -1,11 +1,18 @@
 return {
-  'nvim-orgmode/orgmode',
-  event = 'VeryLazy',
-  ft = { 'org' },
+  "nvim-orgmode/orgmode",
+  event = "VeryLazy",
+  ft = { "org" },
   config = function()
-    require('orgmode').setup({
-      org_agenda_files = '~/org/**/*',
-      org_default_notes_file = '~/org/doc.org',
+    require("orgmode").setup({
+      org_agenda_files = "~/org/**/*",
+      org_default_notes_file = "~/org/doc.org",
+      win_split_mode = "float",
+      org_todo_keywords = { "TODO(t)", "|", "WAITING(w)", "DONE(d)", "CANCELED(c)" },
+      org_todo_keyword_faces = {
+        WAITING = ":foreground blue :weight bold",
+        CANCELED = ":background #FFFFFF :slant italic :underline on",
+        TODO = ":background #000000 :foreground red", -- overrides builtin color for `TODO` keyword
+      },
     })
   end,
 }
