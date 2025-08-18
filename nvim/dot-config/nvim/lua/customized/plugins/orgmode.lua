ebe32b76d60e0c5129fc7412d547b4ef21a93b28
daasity-nathan <nathan@daasity.com>
2025-08-18 14:15:37 -0400

    Updates

--- nvim/dot-config/nvim/lua/customized/plugins/orgmode.lua
+++ nvim/dot-config/nvim/lua/customized/plugins/orgmode.lua
@@ -8,6 +8,8 @@ return {
       org_default_notes_file = "~/Documents/doc.org",
       win_split_mode = "float",
       org_todo_keywords = { "TODO(t)", "|", "WAITING(w)", "DONE(d)", "CANCELED(c)" },
+      org_log_done = false,
+      org_adapt_indentation = false,
       org_todo_keyword_faces = {
         WAITING = ":foreground blue :weight bold",
         CANCELED = ":background #FFFFFF :slant italic :underline on",
